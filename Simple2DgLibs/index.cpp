77c1793ad0e9c56e57593eaea5fb3a165d712dc9
Albert Modenbach <a_modenbach@hotmail.com>
2018-01-06 17:38:57 +0100

    Updated (Test Code 'n stuff)

--- Simple2DgLibs/index.cpp
+++ Simple2DgLibs/index.cpp
@@ -1,13 +1,13 @@
-
-
 #include <string>
 #include "Simple2D.h"
+#include <iostream>
 
 
 extern "C" std::string myStr = "Trololololololololololol";
 
-extern "C" void setup(){
 
+extern "C" void setup(){
+    std::cout << *gameObjects->at(0)->getAttribute<std::string>("myStr") << std::endl;
 }
 
 extern "C" void update(){
