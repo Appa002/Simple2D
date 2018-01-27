f2f8eb0b491457d0e04160ccd8dfebc09b9d243d
Albert Modenbach <a_modenbach@hotmail.com>
2018-01-27 17:35:20 +0100

    Inital Commit

---
+++ Simple2DEngine/header/types.h
@@ -0,0 +1,14 @@
+//
+// Created by albert on 1/27/18.
+//
+
+#ifndef SIMPLE2D_TYPES_H
+#define SIMPLE2D_TYPES_H
+
+namespace Simple2D{
+    struct Vec3{
+        float x,y,z;
+    };
+}
+
+#endif //SIMPLE2D_TYPES_H
