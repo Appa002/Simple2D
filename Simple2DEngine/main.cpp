c694a30e6ea49ae9ec6936b7c7afd999e3671e0e
Albert Modenbach <a_modenbach@hotmail.com>
2018-01-27 14:55:16 +0100

    Removed unnecessary includes

--- Simple2DEngine/main.cpp
+++ Simple2DEngine/main.cpp
@@ -1,10 +1,4 @@
 #include "header/Engine.h"
-#include "header/ExternalCodeLoadingWrapper.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <dlfcn.h>
-#include "header/Map.h"
-#include "header/MapManager.h"
 
 int main(){
     Simple2D::startEngine();
