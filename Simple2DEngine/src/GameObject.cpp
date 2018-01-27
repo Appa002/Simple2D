eab5bf813798a4ab8991ab056d35e00016aaac83
Albert Modenbach <a_modenbach@hotmail.com>
2018-01-27 14:56:00 +0100

    Added sprite rendering functionality

--- Simple2DEngine/src/GameObject.cpp
+++ Simple2DEngine/src/GameObject.cpp
@@ -1,39 +1,37 @@
+#include <stb_image.h>
 #include "../header/GameObject.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "../header/stb_image.h"
+Simple2D::GameObject::GameObject() {
+    spriteHeight = new int(0);
+    spriteWidth = new int(0);
+    vao = new GLuint(0);
+
+    Attribute attr;
+    attr.content = new int(0);
+    attr.hash = typeid(int).hash_code();
+    attr.name = "";
+
+    GameObject::attributes.push_back(attr);
+
+}
+
+Simple2D::GameObject::~GameObject() {
+
+    delete spriteHeight;
+    delete spriteWidth;
+    delete vao;
+
+    GameObject::removeAttribute<int>("");
+    if (!GameObject::attributes.empty())
+        std::cout << "Not all attributes have been removed" << std::endl;
+}
+
 
 
 void Simple2D::GameObject::render(){
-    // Copy Sprite into opengl texture
-    GLuint tex = 0;
-    glGenTextures(1, &tex);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, *x, *y, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
-
-    GLfloat texcoords[]{
-            0.0f, 1.0f,
-            0.0f, 0.0f,
-            1.0f, 0.0f,
-            1.0f, 1.0f,
-            0.0f, 1.0f
-    };
-
-    GLuint vtVbo;
-    glGenBuffers(1, &vtVbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vtVbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(texcoords), texcoords, GL_STATIC_DRAW);
-
-    glBindVertexArray(*vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vtVbo);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
-    glEnableVertexAttribArray(1);
+    if(!imageData)
+        return;
 
 
     GLfloat pos[]{
@@ -57,6 +55,38 @@ void Simple2D::GameObject::render(){
     glEnableVertexAttribArray(0);
 
 
+    // Copy Sprite into opengl texture
+    GLuint tex = 0;
+    glGenTextures(1, &tex);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, *spriteWidth, *spriteHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+
+    GLfloat texcoords[]{
+            0.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 0.0f,
+
+            0.0f, 1.0f,
+            1.0f, 1.0f,
+            1.0f, 0.0f
+    };
+
+    GLuint vtVbo;
+    glGenBuffers(1, &vtVbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vtVbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(texcoords), texcoords, GL_STATIC_DRAW);
+
+    glBindVertexArray(*vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vtVbo);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(1);
+
 
     glBindVertexArray(*vao);
     glDrawArrays(GL_TRIANGLES, 0, 6);
@@ -66,30 +96,30 @@ void Simple2D::GameObject::render(){
 
 void Simple2D::GameObject::preSetup() {
     glGenVertexArrays(1, vao);
+    int n;
+    imageData = stbi_load((path + "/sprite.png").c_str(), spriteWidth, spriteHeight, &n, 4);
 
-    imageData = stbi_load((path + "/sprite.bmp").c_str(), x, y, n, forceChannels);
-    if(!imageData) {
-        printf("GameObject \"%s\" doesn't contain a sprite.png at \"%s\"! \n", name.c_str(), path.c_str());
+    if(!imageData){
+        printf("[WARNING] GameObject \"%s\" does not contain sprite.png at %s \n", name.c_str(), path.c_str());
         return;
     }
 
     // Check if dimensions are not a power of two.
     // Older GPUs can't handel textures which are not a power of two.
-    if((*x & (*x - 1)) != 0 || (*y & (*y - 1)) != 0){
+    if((*spriteWidth & (*spriteWidth - 1)) != 0 || (*spriteWidth & (*spriteHeight - 1)) != 0){
         printf("WARNING: Dimensions not a power of two for GameObject \"%s\" \n", name.c_str());
     }
 
-
     // Filp Images upside down
-    int widthInBytes = 4 * *x;
+    int widthInBytes = 4 * *spriteWidth;
     unsigned char* top = nullptr;
     unsigned char* bottom = nullptr;
     unsigned char tmp = 0;
-    int halfHeight = *y / 2;
+    int halfHeight = *spriteHeight / 2;
 
     for(int row = 0; row < halfHeight; row++){
         top = imageData + row * widthInBytes;
-        bottom = imageData + (*y - row - 1) * widthInBytes;
+        bottom = imageData + (*spriteHeight - row - 1) * widthInBytes;
         for(int col = 0; col < widthInBytes; col++){
             tmp = *top;
             *top = *bottom;
