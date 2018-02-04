ea9773af9cbb038fcc1471a97740fb7d4c1f1234
batburger <a_modenbach@hotmail.com>
2018-02-04 11:29:32 +0100

    Removed various test code

--- testCode/TestObj/index.cpp
+++ testCode/TestObj/index.cpp
@@ -2,26 +2,19 @@
 #include "../../Simple2DEngine/header/Simple2D.h"
 #include "../../Simple2DEngine/header/types.h"
 #include <iostream>
+#include <SDL2/SDL.h>
 
 
 Simple2D::Vec3* position;
 ADD_PROPERTY(Simple2D::Vec3*, position);
-
-void testUpdate(){
-    std::cout << "Sup dawg guess whos being called and shit" << std::endl;
-}
 
 export void init(){
     position = new Simple2D::Vec3(-0.5f, 0.5f, 0);
 }
 
 export void setup(){
-    Simple2D::cloneGameObject(Simple2D::findGameObject("TestObj"), "NewTestObj");
-    Simple2D::findGameObject("NewTestObj")->updatePointer = testUpdate;
 }
 
 export void update(){
-    std::cout << "-";
 }
-
 
