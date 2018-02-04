5541da74a475f7b27cd591c7058d16cf250ca6c7
batburger <a_modenbach@hotmail.com>
2018-02-04 12:34:28 +0100

    Updated Test Code

--- testCode/TestObj/index.cpp
+++ testCode/TestObj/index.cpp
@@ -8,8 +8,12 @@
 Simple2D::Vec3* position;
 ADD_PROPERTY(Simple2D::Vec3*, position);
 
+Simple2D::Vec3* scale;
+ADD_PROPERTY(Simple2D::Vec3*, scale);
+
 export void init(){
     position = new Simple2D::Vec3(-0.5f, 0.5f, 0);
+    scale = new Simple2D::Vec3(1, 1, 1);
 }
 
 export void setup(){
